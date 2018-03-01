246d513604d947ef3532354084eb5b1311464220
Shirish Patel <srpatel3@go.olemiss.edu>
2018-03-01 02:31:07 -0600

    First stage completed

--- Data_Mining/Twitter.py
+++ Data_Mining/Twitter.py
@@ -7,6 +7,7 @@ import os
 import sys
 import csv
 import re
+from collections import Counter
 
 #This function creates and returns an API Object
 def get_API_Object():
@@ -51,26 +52,53 @@ def main():
     min_days_old = 1
     max_days_old = 7
     API_Object = get_API_Object()
-    #csvFile = open('ua.csv', 'a')
-    #Use csv Writer
-    #csvWriter = csv.writer(csvFile)
-    #Now getting tweet ID
-    #max_id = get_tweet_id(API_Object, days_ago=(min_days_old-1))
-    #since_id = get_tweet_id(API_Object, days_ago=(max_days_old-1))
-    #print "max is is", max_id, "and Min ID is : "
-
-    # for tweet in tweepy.Cursor(API_Object.search,q="#healthy",count=100,
+    # csvFile = open('ua.csv', 'a')
+    # #Use csv Writer
+    # csvWriter = csv.writer(csvFile)
+    # #Now getting tweet ID
+    # #max_id = get_tweet_id(API_Object, days_ago=(min_days_old-1))
+    # #since_id = get_tweet_id(API_Object, days_ago=(max_days_old-1))
+    # #print "max is is", max_id, "and Min ID is : "
+    # print "loading"
+    # count = 0
+    # for tweet in tweepy.Cursor(API_Object.search,q="#healthy",
     #                        lang="en",
-    #                        since="2018-02-27").items():
-    #     print (tweet.created_at, tweet.text)
+    #                        since="2018-02-22").items():
+    #     #print (tweet.created_at, tweet.text)
+    #     print count
+    #     count = count+1
+    #     if count > 700:
+    #         print "Sleeping"
+    #         time.sleep(20)
+    #         count = 0
     #     csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8')])
 
+    # Let's start processing this shit
+    list_hashtags = []
     with open('ua.csv','rb') as csvfile:
         #line = csv.reader(csvfile, delimiter=',')
-        while line = csvfile.readline():
-            print line
+        #for line in csvfile:
+        for i in range(0,10):
+            line = csvfile.readline()
+            line = line.lower()
             hashtags = re.findall(r"#(\w+)",line)
-            print hashtags
+            for tags in hashtags:
+                # print tags
+                # if tags not in list_hashtags:
+                list_hashtags.append(tags)
+
+
+
+        items = dict(Counter(list_hashtags))
+        #sorted(student_tuples, key=lambda student: student[2])
+
+        dictlist = []
+        for key, value in items.iteritems():
+            temp = [key,value]
+            dictlist.append(temp)
+        dictlist1 = sorted(dictlist, key=lambda tag: tag[1],reverse = True)
+        print dictlist1
+
 
 if __name__ == "__main__":
     main()
