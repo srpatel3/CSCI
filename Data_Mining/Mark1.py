e4f9e190cfa8fd7fe9d4f6c297449b1e8385446d
srpatel3 <srpatel3@go.olemiss.edu>
2018-05-01 21:54:15 -0500

    Mark3 added

--- Data_Mining/Mark1.py
+++ Data_Mining/Mark1.py
@@ -1,15 +1,23 @@
 import random
 
-fileToRead = open("twitter_csci581.csv","r")
-print "Reading File"
-inputData = fileToRead.readlines()
-fileToRead.close()
-print "Shuffling Data"
-random.shuffle(inputData)
-print "Shuffling Done."
-fileToWrite = open("twitter_csci581_revised.csv","w")
+# fileToRead = open("twitter_csci581.csv","r")
+# print "Reading File"
+# inputData = fileToRead.readlines()
+# fileToRead.close()
+# print "Shuffling Data"
+# random.shuffle(inputData)
+# print "Shuffling Done."
+# fileToWrite = open("twitter_csci581_revised.csv","w")
+#
+# print "Now Writing Data to file"
+# fileToWrite.writelines(inputData)
+# print "Writing Done."
+# fileToWrite.close()
 
-print "Now Writing Data to file"
-fileToWrite.writelines(inputData)
-print "Writing Done."
-fileToWrite.close()
+
+
+def main():
+    print "Hii inside main"
+
+if __name__ == "__main__":
+    main()
