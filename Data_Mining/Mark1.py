638eb688a3ee20aac664be7deb517160925bd3a4
srpatel3 <srpatel3@go.olemiss.edu>
2018-05-03 18:13:30 -0500

    Mark 3

--- Data_Mining/Mark1.py
+++ Data_Mining/Mark1.py
@@ -1,23 +1,128 @@
 import random
+import pandas as pd
 
-# fileToRead = open("twitter_csci581.csv","r")
-# print "Reading File"
-# inputData = fileToRead.readlines()
-# fileToRead.close()
-# print "Shuffling Data"
-# random.shuffle(inputData)
-# print "Shuffling Done."
-# fileToWrite = open("twitter_csci581_revised.csv","w")
-#
-# print "Now Writing Data to file"
-# fileToWrite.writelines(inputData)
-# print "Writing Done."
-# fileToWrite.close()
+def printstuff():
+    print "Hello"
+
+# takes two argument one is the dictionary and another is bag of words for doc
+def computeTF(worDict, bow):
+    tfDict = {}
+    totalWordsInBag = len(bow)
+    for word,count in worDict.iteritems():
+        tfDict[word] = count/float(totalWordsInBag)
+    return tfDict
 
 
+# Should be able to calculate IDF
 
-def main():
-    print "Hii inside main"
+def computeIDF(dictList):
+    import math
+    idfDict = {}
+    numOfDocs = len(dictList)
+    # initize with words from first document
+    idfDict = dict.fromkeys(dictList[0].keys(),0)
+    for dictionary in dictList:
+        for word, value in dictionary.iteritems():
+            if value > 0:
+                idfDict[word] += 1
 
-if __name__ == "__main__":
-    main()
+    for word, value in idfDict.iteritems():
+        idfDict[word] = math.log(numOfDocs / float(value))
+
+    return idfDict
+
+# Now would be the good time to Compute TFIDF
+def computeTDIDF(tfBow,idfs):
+    tfidf = {}
+    for word, value in tfBow.iteritems():
+        tfidf[word] = value * idfs[word]
+
+    return tfidf
+
+
+# Actually deals with clustering
+def documentClustering():
+    docA = "this cat sat on my face"
+    docB = "this god sat on my bed"
+    docC = "this cat sit on my bed"
+    bowA = docA.split(" ")
+    bowB = docB.split(" ")
+    bowC = docC.split(" ")
+    listBow = []
+    listBow.append(bowA)
+    listBow.append(bowB)
+    listBow.append(bowC)
+    vocab = set()
+    for bow in listBow:
+        vocab = vocab.union(set(bow))
+
+    listDict = []
+    for i in range(0,len(listBow)):
+        listDict.append(dict.fromkeys(vocab,0))
+
+    for i in range(0,len(listBow)):
+        for word in listBow[i]:
+            listDict[i][word] += 1
+
+    listtfBow = []
+    for i in range(0,len(listBow)):
+        listtfBow.append(computeTF(listDict[i],listBow[i]))
+
+    idfs = computeIDF(listDict)
+
+    # Actually calculating TFIDF
+    listTFIDF = []
+    for i in range(0,len(listBow)):
+        listTFIDF.append(computeTDIDF(listtfBow[i],idfs))
+
+    print pd.DataFrame(listTFIDF)
+
+
+
+
+
+
+
+
+
+# Original Approach
+# def documentClustering():
+    # docA = "this cat sat on my face"
+    # docB = "this god sat on my bed"
+    # docC = "this cat sit on my bed"
+    # bowA = docA.split(" ")
+    # bowB = docB.split(" ")
+    # bowC = docC.split(" ")
+    # print bowA
+    # print bowB
+    # vocab = set()
+    # for bow in listBow:
+    #     vocab = vocab.union(set(bow))
+    # print vocab
+    # dictA = dict.fromkeys(vocab,0)
+    # dictB = dict.fromkeys(vocab,0)
+    # dictC = dict.fromkeys(vocab,0)
+    # print dictA
+    # for  bow in listBow:
+    # for word in bowA:
+    #     dictA[word] += 1
+    #
+    # for word in bowB:
+    #     dictB[word] += 1
+    # for word in bowC:
+    #     dictC[word] += 1
+    # print dictA
+    # print dictB
+    # Computing TF
+    # print pd.DataFrame([dictA,dictB])
+    # tfbowA = computeTF(dictA, bowA)
+    # tfbowB = computeTF(dictB, bowB)
+    # tfbowC = computeTF(dictC, bowC)
+    # Computing IDF
+    # print pd.DataFrame([tfbowA,tfbowB])
+    # idfs = computeIDF(listDict)
+    # Actually calculating TFIDF
+    # tfidfBowA = computeTDIDF(tfbowA,idfs)
+    # tfidfBowB = computeTDIDF(tfbowB, idfs)
+    # tfidfBowC = computeTDIDF(tfbowC, idfs)
+    # print pd.DataFrame(listTFIDF)
--- Data_Mining/Mark3.py
+++ Data_Mining/Mark3.py
@@ -1,4 +1,5 @@
 import os
+import Mark1
 import string
 import operator
 from nltk.tokenize import word_tokenize as tokens
@@ -18,38 +19,39 @@ def getDict(readFile,temp):
 def main():
     # print "Hii inside main"
     path = "/home/shirish/Data_Files/Machine_Learning/20news-18828/"
-    dirs = os.walk(path)
-    count = 0
-    directory_Dicts = []
-    dirList = []
-    for roots,dirs,files in os.walk(path):
-        directory = dirs
-        for dirs in directory:
-            temp = {}
-            dirList.append(dirs)
-            for files in os.walk(path+"/"+dirs):
-                fileList = files[2]
-                for files in fileList:
-                    readFile = open(path+"/"+dirs+"/"+files,"r")
-                    getDict(readFile,temp)
-            directory_Dicts.append(temp)
-        break
-    dir_Dict = {}
-    for i in range(0,len(dirList)):
-        dir_Dict[dirList[i]] = len(directory_Dicts[i])
-    dir_Dict = list(sorted(dir_Dict.items(), key=lambda x:x[1], reverse=True))
-    for item in dir_Dict:
-        print item
-
-    directory_Dicts.sort(reverse=True,key= len)
-
-    # for i in range(0,5):
-        # print len(directory_Dicts[i])
-        # print directory_Dicts[i]
-
-    for item in directory_Dicts:
-        print len(item)
-    # print len(directory_Dicts[0])
+    # Mark1.documentClustering()
+    # dirs = os.walk(path)
+    # count = 0
+    # directory_Dicts = []
+    # dirList = []
+    # for roots,dirs,files in os.walk(path):
+    #     directory = dirs
+    #     for dirs in directory:
+    #         temp = {}
+    #         dirList.append(dirs)
+    #         for files in os.walk(path+"/"+dirs):
+    #             fileList = files[2]
+    #             for files in fileList:
+    #                 readFile = open(path+"/"+dirs+"/"+files,"r")
+    #                 getDict(readFile,temp)
+    #         directory_Dicts.append(temp)
+    #     break
+    # dir_Dict = {}
+    # for i in range(0,len(dirList)):
+    #     dir_Dict[dirList[i]] = len(directory_Dicts[i])
+    # dir_Dict = list(sorted(dir_Dict.items(), key=lambda x:x[1], reverse=True))
+    # for item in dir_Dict:
+    #     print item
+    #
+    # directory_Dicts.sort(reverse=True,key= len)
+    #
+    # # for i in range(0,5):
+    #     # print len(directory_Dicts[i])
+    #     # print directory_Dicts[i]
+    #
+    # for item in directory_Dicts:
+    #     print len(item)
+    # # print len(directory_Dicts[0])
 
 if __name__ == "__main__":
     main()
